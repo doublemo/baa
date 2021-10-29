c3c15ec12e60f484fd5f35af6fd8da9d48f49f59
randyma <435420057@qq.com>
2021-10-29 12:25:54 +0800

    调整错误码生成方式

--- internal/proto/kit/kit.go
+++ internal/proto/kit/kit.go
@@ -15,6 +15,26 @@ const (
 )
 
 // MakeErrCode 为不同组件创建对应的错误代码
-func MakeErrCode(cmd coresproto.Command, seq int32) int32 {
-	return cmd.Int32()*10000 + seq
+// * <pre>{@code
+// * +--------------------------+------------+
+// * |     kit id    | sequence |   extends  |
+// * +--------------------------+------------+
+// *       16bits       13bits       2bits
+// * }</pre>
+//  kit -32767 ~ 32767
+//  seq 0 ~ 4096
+//  extends ：0 ~ 3
+func MakeErrCode(cmd coresproto.Command, seq uint32, extends ...uint32) int32 {
+	if len(extends) < 1 {
+		extends = []uint32{0}
+	}
+	return (int32(cmd) << uint(16)) | (int32(seq) << uint(3)) | int32(extends[0])
+}
+
+// ParseErrCode 解析errcode
+func ParseErrCode(code int32) (int32, uint32, uint32) {
+	extends := (code << uint(29)) >> uint(29)
+	sequence := (code << uint(16)) >> uint(19)
+	kit := code >> 16
+	return kit, uint32(sequence), uint32(extends)
 }
---
+++ internal/proto/kit/kit_test.go
@@ -0,0 +1,23 @@
+package kit
+
+import (
+	"testing"
+
+	coresproto "github.com/doublemo/baa/cores/proto"
+)
+
+func TestMakeErrCode(t *testing.T) {
+	t.Log(MakeErrCode(USRT, 1))
+	for i := -32767; i < 32767; i++ {
+		for j := 1; j < 4096; j++ {
+			for k := 0; k < 4; k++ {
+				code := MakeErrCode(coresproto.Command(i), uint32(j), uint32(k))
+				pi, pj, pk := ParseErrCode(code)
+				if pi != int32(i) || pj != uint32(j) || pk != uint32(k) {
+					t.Fatalf("code:%d, i = %d, j = %d, k = %d  != pi =%d pj = %d pk != %d", code, i, j, k, pi, pj, pk)
+				}
+			}
+		}
+	}
+
+}
